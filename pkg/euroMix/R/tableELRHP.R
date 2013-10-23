9e5ad416d00c6ffaaf07ef90c6e19316bac66fbc
gdorum <gdorum@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-23 14:31:59 +0000

    Lagt inn LRmoments, convertToFamilias og oppdatert tableELRHP.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/euromix@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/euroMix/R/tableELRHP.R
+++ pkg/euroMix/R/tableELRHP.R
@@ -1,13 +1,11 @@
-tableELRHP=function(L=4){
+tableELRHP=function(L=4,p=rep(1/L,L)){
 res=NULL
-p=rep(1,L)
 p=p/sum(p)
 q=q012(p=p)
 probHD=qkappa(kappa=c(1,0.0,0.0),q=q)
 k0=c(1,0,0,0.25,0.5,0.75,9/16,17/32)
 k1=c(0,1,0,0.5,0.5,0.25,6/16,14/32)
 k2=c(0,0,1,0.25,0,0,1/16,1/32)
-phi=(2*k2+k1)/4
 kappaMat=cbind(k0,k1,k2)
 for (i in 1:dim(kappaMat)[1]){
 k0=as.double(kappaMat[i,1]);k1=as.double(kappaMat[i,2]);k2=as.double(kappaMat[i,3])
@@ -17,11 +15,11 @@ c=k2^2/2
 a=1-b-c
 exact2=a+b*L+c*L^2
 probHP=qkappa(kappa=c(k0,k1,k2),q=q)
-LR.num=sum((probHP)^2/probHD)
-ledd=c(k0=k0,k1=k1,k2=k2,formel2=exact2,num=LR.num)
+LR2=sum(probHP^3/probHD^2)
+ledd=c(k0=k0,k1=k1,k2=k2,E.LR.HP=exact2,var.LR.HP=LR2-exact2^2,var.LR.HD=exact2-1)
 res=rbind(res,ledd)
 }
-nn=c("Unrelated","Parent-offspring","Monozygous-twin","Full-Sib","Half-sib etc","First cousin","Double first-cousin","Quadruple...")
+nn=c("Unrelated","PI","Monozygous-twin","SI","HSI","FC","DFC","QHFC")
 rownames(res)=nn
 res
 }
