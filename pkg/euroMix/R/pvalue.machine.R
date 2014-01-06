a001c1b94c73beb337c89fdd6e97f3458f766953
gdorum <gdorum@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-01-06 09:22:36 +0000

    Fjernet pvalue.machine med R-kode. Kun versjon med C-kode igjen
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/euromix@34 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/euroMix/R/pvalue.machine.R
+++ pkg/euroMix/R/pvalue.machine.R
@@ -1,14 +1,21 @@
 
-
-
 pvalue.machine <- function( LR.suspect, LR.table, P.table ){
 #
 # Computes the p-value for LR.suspect
 #
 # Input:
 # LR.suspect  - The likelihood ratio for the suspect genotype profile (1x1 positive value)
-# LR.table    - The pre-computed likelihood ratios for every genotype of every marker (MxG matrix)
+# LR.table    - The pre-computed likelihood ratios for every genotype of every marker (MxG matrix).
+#               Each row corresponds to a marker. G is the maximum number of genotypes for any marker. 
+#               Markers with fewer than G genotypes must have 0 in redundant columns.
 # P.table     - The population probabilities for every genotype of every marker (MxG matrix)
+#               Must corresponds to the genotypes in LR.table. See description of LR.table.
+  
+  #sys <- Sys.info()[1]
+  #if(sys=="Windows") file <- system.file("files/prodrecfunction.dll", package = "euroMix")
+  #if(sys=="Darwin") file <- system.file("files/prodrecfunction.so", package = "euroMix")
+  #else stop("Unknown system")
+  #dyn.load(file)
   
   # The number of markers
   M <- nrow( LR.table )
@@ -16,70 +23,39 @@ pvalue.machine <- function( LR.suspect, LR.table, P.table ){
   # Preparing input
   prepped <- .prepare.input( LR.table, P.table )
   LR.list <- prepped$LR.list
-  P.list  <- prepped$P.list
-  inflation.factor <- prepped$Inflation.factor
+  Plist  <- as.numeric(unlist(prepped$P.list))
+  inflationfactor <- as.numeric(prepped$Inflation.factor)
   
   # The number of genotypes for each marker
-  G <- sapply( LR.list, length )
+  G <- as.integer(sapply( LR.list, length ))
 
   # Initial values
-  marker <- 1
-  score <- 1.0
-  pvalue <- 1.0
-  
-  p.value <- .recfun( marker, score, pvalue, LR.suspect, LR.list, P.list, M, G, inflation.factor )
+  pvalue <- as.numeric(1)
+  LRsuspect  = as.numeric(LR.suspect)
+  LRlist <- as.numeric(unlist(LR.list))
+ 
+  p.value <- .C("prodrecfunction",pvalue, LRsuspect, LRlist,Plist,M,G,inflationfactor)[[1]]
+  #p.value <- .C("prodrecfunction",pvalue, LRsuspect, LRlist,Plist,M,G,inflationfactor,PACKAGE="euroMix")[[1]]
   return( p.value )
 }
 
 
-.recfun <- function( marker, score, pvalue, LR.suspect, LR.list, P.list, M, G, inflation.factor ){
-#
-# The recursive function that computes the p-value.
-#
-  
-  if( marker == M ){ # The terminating case, we are at the last marker
-    pvals <- rep( 0, G[marker] ) # There are G[m] genotypes of this marker, all must have 0 p-value until we know they give score above LR.suspect
-    for( genotype in 1:G[marker] ){ # Looping over all genotypes, compute LR-score, and p-value if score is above LR.suspect
-      if( score * LR.list[[marker]][genotype] >= LR.suspect ){
-        pvals[genotype] <- pvalue * P.list[[marker]][genotype]
-      } else {
-        break; # Breaking out of the loop, no need to compute more scores since genotypes are sorted in descending order
-      }
-    }
-    return( sum( pvals ) ) # This is the p-value for the path leading here
-  } else { # We are not yet at the final marker
-    pvals <- rep( 0, G[marker] ) # There are G[m] genotypes of this marker, all must have 0 p-value until we know they give score above LR.suspect
-    for( genotype in 1:G[marker] ){
-      score.new <- score * LR.list[[marker]][genotype]
-      pvalue.new <- pvalue * P.list[[marker]][genotype]
-      if( score.new * inflation.factor[marker+1] >= LR.suspect ){ # Give up further recursions if we cannot inflate current score above LR.suspect
-        pvals[genotype] <- .recfun( marker+1, score.new, pvalue.new, LR.suspect, LR.list, P.list, M, G, inflation.factor ) # We dig one marker deeper into the tree...
-      } else {
-        break; # Breaking out of loop and terminating recursion because we cannot inflate current score
-      }
-    }
-    return( sum( pvals ) ) # This is the p-value for the path leading here
-  }
-}
-
-
-
 .prepare.input <- function( LR.table, P.table ) {
-#
-# Converting the tables of likelihood ratios and probabilities to lists,
-# keeping only the unique LR-scores for each marker, and sorting everything
-# in proper order.
-#
-# Input:
-# LR.table  - Matrix of likelihood ratio scores for all genotypes of all markers. 
-#             Each row correspond to a marker, and it may contain 0's (MxG matrix)
-# P.table   - Matrix of population probabilities for all genotypes of all markers.
-#             Each row correspond to a marker, and it may contain 0's (MxG matrix)
-#
-# The function returns a list containing two lists, LR.list and P.list, containing the unique 
-# non-zero values in LR.table and P.table. For each marker the elements are sorted in descending
-# order by LR-score.
-#
+  #
+  # Converting the tables of likelihood ratios and probabilities to lists,
+  # keeping only the unique LR-scores for each marker, and sorting everything
+  # in proper order.
+  #
+  # Input:
+  # LR.table  - Matrix of likelihood ratio scores for all genotypes of all markers. 
+  #             Each row correspond to a marker, and it may contain 0's (MxG matrix)
+  # P.table   - Matrix of population probabilities for all genotypes of all markers.
+  #             Each row correspond to a marker, and it may contain 0's (MxG matrix)
+  #
+  # The function returns a list containing two lists, LR.list and P.list, containing the unique 
+  # non-zero values in LR.table and P.table. For each marker the elements are sorted in descending
+  # order by LR-score.
+  #
   M <- nrow( LR.table ) # The number of markers
   LR.list <- P.list <- vector( "list", M )
   LR.max <- apply( LR.table, 1, max )
