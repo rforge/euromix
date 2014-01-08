7c0faed3582fc3fdf3a2526e2e269a064947fadb
gdorum <gdorum@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-01-08 13:11:11 +0000

    Endret innlesing i LRpvalue. Kun autosomale markører tillatt.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/euromix@38 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/euroMix/R/LRpvalue.R
+++ pkg/euroMix/R/LRpvalue.R
@@ -25,28 +25,35 @@ LRpvalue <- function(samplefile, victimfile, suspectfile, freqfile, hp, hd, prD,
   }
   
   #Sample data
+  #With AMEL
   #Read first line of file to check how many columns there are
-  testread <- scan(samplefile, what='character', nlines=1, sep=",",quiet=TRUE)
-  sampleDataAll <- read.table(samplefile,sep=",",header=TRUE,na.strings=c("NA","na","NaN","X","Y"),
-                              colClasses=c('character','character',rep('numeric',length(testread)-2)))
-  ix <- grep('AMEL',sampleDataAll[,2])
-  if(length(ix) > 0) sampleDataAll <- sampleDataAll[-ix,]
+  #testread <- scan(samplefile, what='character', nlines=1, sep=",",quiet=TRUE)
+  #sampleDataAll <- read.table(samplefile,sep=",",header=TRUE,na.strings=c("NA","na","NaN","X","Y"),
+  #                            colClasses=c('character','character',rep('numeric',length(testread)-2)))
+  #ix <- grep('AMEL',sampleDataAll[,2])
+  #if(length(ix) > 0) sampleDataAll <- sampleDataAll[-ix,]
+  sampleDataAll <- read.table(samplefile,sep=",",header=TRUE)
   sampleData <- sampleDataAll[,3:ncol(sampleDataAll)]
   rownames(sampleData) <- sampleDataAll[,2]
   
   #Victim profile
-  victimDataAll <- read.table(victimfile,sep=",",header=TRUE,na.strings=c("NA","na","NaN","X","Y"),
-                              colClasses=c('character','character','numeric','numeric'))
-  ix <- grep('AMEL',victimDataAll[,2])
-  if(length(ix) > 0) victimDataAll <- victimDataAll[-ix,]
+  #With AMEL
+#   victimDataAll <- read.table(victimfile,sep=",",header=TRUE,na.strings=c("NA","na","NaN","X","Y"),
+#                               colClasses=c('character','character','numeric','numeric'))
+#   ix <- grep('AMEL',victimDataAll[,2])
+#   if(length(ix) > 0) victimDataAll <- victimDataAll[-ix,]
+  victimDataAll <- read.table(victimfile,sep=",",header=TRUE)
   victimData <- victimDataAll[,3:ncol(victimDataAll)]
   rownames(victimData) <- victimDataAll[,2]  
   
   #Suspect profile(s)
-  suspectDataAll <- read.table(suspectfile,sep=",",header=TRUE,na.strings=c("NA","na","NaN","X","Y"),
-                               colClasses=c('character','character','numeric','numeric'))
-  ix <- grep('AMEL',suspectDataAll[,2])
-  if(length(ix) > 0) suspectDataAll <- suspectDataAll[-ix,]
+  #With AMEL marker
+  #suspectDataAll <- read.table(suspectfile,sep=",",header=TRUE,na.strings=c("NA","na","NaN","X","Y"),
+  #                             colClasses=c('character','character','numeric','numeric'))
+  #ix <- grep('AMEL',suspectDataAll[,2])
+  #if(length(ix) > 0) suspectDataAll <- suspectDataAll[-ix,]
+  #Without AMEL
+  suspectDataAll <- read.table(suspectfile,sep=",",header=TRUE)
   suspectData <- split(suspectDataAll[,3:4],suspectDataAll[,1])
   for(i in 1:length(suspectData)) rownames(suspectData[[i]]) <- suspectDataAll[1:nrow(suspectData[[1]]),2]
   
